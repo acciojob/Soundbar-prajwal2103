d3917d18d7a55332453df4d5cb0e2f29befbb215
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2024-04-07 20:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const btn=document.querySelector(".btn");
+function soundPlay(){
+	let audio=document.getElementById("audio")
+	audio.src= "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+	audio.play();
+	if(audio){
+		stop.addEventListener("click",()=>{
+			audio.pause();
+		});
+	}
+}
+btn.addEventListener("click",soundPlay);
+const stop=document.querySelector(".stop");
+
+
